2f2d2a7de2542515d66b242307cd922756fc975f
WreckerB <imagofx05@gmail.com>
2023-08-06 00:39:59 +0800

    update project

--- assets/index-21a5e227.js
+++
@@ -1,1 +0,0 @@
-import{d as s,r as t,aN as a,aO as o,aP as c}from"./index-7680faf0.js";const n={class:"h-full"},r=["src"],u=s({__name:"index",setup(l){const e=t("https://www.naiveui.com/zh-CN/os-theme/docs/introduction");return(i,_)=>(a(),o("div",n,[c("iframe",{class:"wh-full",src:e.value},null,8,r)]))}});export{u as default};
