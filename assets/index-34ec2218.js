9f727720d5fad9a987b06088bf8f7ad6f96c38b0
WreckerB <imagofx05@gmail.com>
2023-08-06 16:32:06 +0800

    update project

--- assets/index-34ec2218.js
+++
@@ -1,1 +0,0 @@
-import{d as a,aN as e,aQ as n,aS as o}from"./index-5975c133.js";import{_ as r}from"./index.vue_vue_type_script_setup_true_lang-797ccfb7.js";/* empty css                                                         */const f=a({name:"BlankLayout",__name:"index",setup(s){return(t,_)=>(e(),n(o(r),{"show-padding":!1}))}});export{f as default};
---
+++ assets/use-compitable-8474c2e2.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-7dc8ada0.js";function f(n,o){return t(()=>{for(const r of o)if(n[r]!==void 0)return n[r];return n[o[o.length-1]]})}export{f as u};
